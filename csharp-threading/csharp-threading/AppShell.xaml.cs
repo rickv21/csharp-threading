593d7e07629c2f851587a1a558a77ee181246c6f
BroederToon <thijs.janse1@student.nhlstenden.com>
2024-02-27 23:35:54 +0100

    added MVVM architecture

---
+++ csharp-threading/csharp-threading/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace csharp_threading;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ csharp-threading/csharp-threading/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace csharp_threading;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
