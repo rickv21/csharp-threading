f1c15f90068e3e65a2f83307b9941d8369fba98b
rickv21 <rick.vinke@student.nhlstenden.com>
2024-03-01 10:19:06 +0100

    Added ConfigModel.
    - Added test code for the ConfigModel.
    - Added Newtonsoft.Json as a dependency.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,21 @@
-﻿namespace FileManager
+﻿using FileManager.Models;
+
+namespace FileManager
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        ConfigModel configModel;
 
         public MainPage()
         {
             InitializeComponent();
+            //Config test code.
+            configModel = ConfigModel.LoadOrCreateDefault();
+
+            TitleLabel.Text = configModel.Option1;
+            count = configModel.ClickCount;
+            CounterBtn.Text = $"Clicked {count} time";
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -19,6 +28,8 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+            configModel.ClickCount = count;
+            configModel.Save();
         }
     }
 
