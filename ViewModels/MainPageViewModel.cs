730b872bcb32192be40ced015c20b6e80fe67d5a
rickv21 <rick.vinke@student.nhlstenden.com>
2024-03-12 14:27:53 +0100

    Fixed config for current MVVM layout.

--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -7,23 +7,47 @@ namespace FileManager.ViewModels
     public class MainPageViewModel : INotifyPropertyChanged
     {
         private Count _count;
-        
+        private ConfigModel configModel;
 
         public string CounterText => $"Clicked {_count.Value} {(_count.Value == 1 ? "time" : "times")}";
 
+        private string _labelText;
+        public string LabelText
+        {
+            get { return _labelText; }
+            set
+            {
+                if (_labelText != value)
+                {
+                    _labelText = value;
+                    OnPropertyChanged(nameof(LabelText));
+                }
+            }
+        }
+
         public ICommand IncrementCountCommand { get; }
+
+
 
         public MainPageViewModel()
         {
+            configModel = ConfigModel.LoadOrCreateDefault();
+            LabelText = configModel.Option1;
             IncrementCountCommand = new Command(IncrementCount);
             _count = new Count();
+            _count.Value = configModel.ClickCount;
+
             PropertyChanged += (sender, args) =>
             {
                 if (args.PropertyName == nameof(Count.Value))
                 {
                     OnPropertyChanged(nameof(CounterText));
                 }
+
             };
+
+ 
+
         }
 
         private void IncrementCount()
@@ -32,6 +56,8 @@ namespace FileManager.ViewModels
             SemanticScreenReader.Announce(CounterText);
             OnPropertyChanged(nameof(CounterText));
         }
+
+
 
         public event PropertyChangedEventHandler PropertyChanged;
 
