84ee85139e05f166b5d412c84149783a4d691129
rickv21 <rick.vinke@student.nhlstenden.com>
2024-03-16 12:20:44 +0100

    Files can now be opened by double clicking.
    The path is now shows above the filelist.
    Changed the path label to a entry field.
    Made the directory loading parallel to help speed up the loading (it did not).

--- ViewModels/FileOverviewViewModel.cs
+++ ViewModels/FileOverviewViewModel.cs
@@ -1,6 +1,9 @@
+using System.Collections.Concurrent;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
+using System.Runtime;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using FileManager.Models;
@@ -19,12 +22,21 @@ public class FileOverviewViewModel : INotifyPropertyChanged
             _files = value;
         }
     }
+    public String CurrentPath
+    {
+        get { return _currentPath; }
+        set
+        {
+            _currentPath = value;
+            OnPropertyChanged(nameof(CurrentPath));
+        }
+    }
     public event PropertyChangedEventHandler? PropertyChanged;
     public ICommand ItemDoubleTappedCommand { get; }
 
     public FileOverviewViewModel()
     {
-        _currentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        CurrentPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
         DirectoryInfo d = new DirectoryInfo(_currentPath);
         _files = new ObservableCollection<Item>();
 
@@ -35,49 +47,65 @@ public class FileOverviewViewModel : INotifyPropertyChanged
     void OnItemDoubleTapped(Item item)
     {
         System.Diagnostics.Debug.WriteLine("Testing - " + item.FilePath);
-        if(item.FileName == "...")
+        if (item is DirectoryItem)
         {
-        
-            DirectoryInfo directoryInfo = Directory.GetParent(_currentPath);
-            _currentPath = directoryInfo.FullName;
-            _files.Clear();
-            FillList(directoryInfo);
-            return;
-        }
-        if (Directory.Exists(item.FilePath))
+            if (item.FileName == "...")
+            {
+                DirectoryInfo directoryInfo = Directory.GetParent(_currentPath);
+                CurrentPath = directoryInfo.FullName;
+                _files.Clear();
+                FillList(directoryInfo);
+                return;
+            }
+            if (Directory.Exists(item.FilePath))
+            {
+                System.Diagnostics.Debug.WriteLine("Exists");
+                // If the item is a folder, update the Files collection to show the contents of the folder
+                DirectoryInfo directoryInfo = new DirectoryInfo(item.FilePath);
+                CurrentPath = item.FilePath;
+                _files.Clear();
+                FillList(directoryInfo);
+                return;
+            }
+        } else if(item is FileItem)
         {
-            System.Diagnostics.Debug.WriteLine("Exists");
-            // If the item is a folder, update the Files collection to show the contents of the folder
-            DirectoryInfo directoryInfo = new DirectoryInfo(item.FilePath);
-            _currentPath = item.FilePath;
-            _files.Clear();
-            FillList(directoryInfo);
+            FileInfo fileInfo = new FileInfo(item.FilePath);
+            Process.Start(new ProcessStartInfo(fileInfo.FullName) { UseShellExecute = true });
         }
     }
 
 
     private void FillList(DirectoryInfo d)
     {
-        _files.Add(new DirectoryItem("...", "", 0));
+        if (d.Parent != null)
+        {
+            _files.Add(new DirectoryItem("...", "", 0));
+        }
         try
         {
 
-            foreach (FileSystemInfo item in d.GetFileSystemInfos())
+            ConcurrentBag<Item> fileSystemInfos = new ConcurrentBag<Item>();
+
+            Parallel.ForEach(d.EnumerateDirectories(), dir =>
             {
-                System.Diagnostics.Debug.WriteLine(item.FullName);
-                if ((item.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
-                {
-                    // It's a directory
-                    DirectoryInfo dirInfo = new DirectoryInfo(item.FullName);
-                    _files.Add(new DirectoryItem(dirInfo.Name, dirInfo.FullName, 0));
-                }
-                else
-                {
-                    // It's a file
-                    FileInfo fileInfo = new FileInfo(item.FullName);
-                    _files.Add(new FileItem(fileInfo.Name, fileInfo.FullName, 0, ""));
-                }
+                // It's a directory
+                DirectoryInfo dirInfo = new DirectoryInfo(dir.FullName);
+                fileSystemInfos.Add(new DirectoryItem(dirInfo.Name, dirInfo.FullName, 0));
+            });
+
+            Parallel.ForEach(d.EnumerateFiles(), file =>
+            {
+                FileInfo fileInfo = new FileInfo(file.FullName);
+                fileSystemInfos.Add(new FileItem(fileInfo.Name, fileInfo.FullName, 0, ""));
+            });
+
+       
+
+            foreach (var item in fileSystemInfos.OrderBy(fsi => !(fsi is DirectoryInfo)).ThenBy(fsi => fsi.FileName))
+            {
+                _files.Add(item);
             }
+
         } catch {
         //Check for no permission.
         
