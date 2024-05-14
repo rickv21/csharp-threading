e498443f5ee2b72e14e92073d8affdc1d1693b83
rickv21 <89781098+rickv21@users.noreply.github.com>
2024-05-14 10:25:30 +0200

    Fixed size not displaying for files that are 0 bytes.

--- Models/FileItem.cs
+++ Models/FileItem.cs
@@ -6,10 +6,7 @@
         public FileItem(string fileName, string filePath, long size, String type, ImageSource icon, short side, Boolean hidden, DateTime lastEditDateTime) : base(fileName, filePath, icon, side, hidden)
         {
             Size = size;
-            if (size > 0)
-            {
-                ReadableSize = FileUtil.ConvertBytesToHumanReadable(Size);
-            }
+            ReadableSize = FileUtil.ConvertBytesToHumanReadable(Size);
             LastEdited = lastEditDateTime;
             FileInfo = type + " " + FileInfo;
             Type = ItemType.File;
